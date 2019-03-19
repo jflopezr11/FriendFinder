48532548f122b4ca251755ed2bebe3c18e1febe5
Joshua Lopez-Roque <joshualopezroque@gmail.com>
2019-03-18 19:34:51 -0700

    finish

---
+++ app/data/friends.js
@@ -0,0 +1,38 @@
+var friends = [
+    {
+        name: "Tim Drake",
+        photo: "https://nl.wikipedia.org/wiki/Tim_Drake#/media/File:Tim_Drakeawkwardnessisavirtue.jpg",
+        scores: [3, 2, 2, 5, 5, 3, 1, 3, 5, 5]
+    },
+    {
+        name:"Raven",
+        photo: "https://www.dccomics.com/sites/default/files/imce/2017/08-AUG/Raven_TITANS%20TV%20CASTING_v03_r01_59839e363646d5.95096872.jpg",
+        scores: [2, 5, 4, 3, 3, 2, 3, 2, 2, 2]
+    },
+    {
+        name:"Donna Troy",
+        photo: "https://en.wikipedia.org/wiki/Donna_Troy#/media/File:DonnaTroy.jpg",
+        scores:[2, 3, 3, 5, 2, 1, 4, 1, 4, 5]
+    },
+    {
+        name:"Miss Martian",
+        photo: "https://upload.wikimedia.org/wikipedia/en/2/2d/Miss_Martian.jpg",
+        scores:[5, 2, 4, 3, 2, 1, 2, 3, 3, 1]
+    },
+    {
+        name:"Wallace West",
+        photo: "https://www.dccomics.com/sites/default/files/GalleryComics_1920x1080_20180131-v1_0007_flash_ann_1_5a73d50b57a959.37685172.jpg",
+        scores:[1, 4, 1, 4, 5, 5, 1, 5, 2, 1]
+    },
+    {
+        name:"Roy Harper",
+        photo: "https://vignette.wikia.nocookie.net/marvel_dc/images/4/46/Arsenal_0012.jpg/revision/latest/scale-to-width-down/350?cb=20190121063442",
+        scores:[4, 3, 4, 4, 3, 5, 4, 3, 4, 3]
+    },
+    {
+        name:"Kyle Rayner",
+        photo: "https://vignette.wikia.nocookie.net/marvel_dc/images/3/35/Kyle_Rayner_06.jpg/revision/latest/scale-to-width-down/323?cb=20101009181628",
+        scores:[3, 1, 2, 2, 5, 1, 5, 4, 4, 2]
+    },
+];
+
